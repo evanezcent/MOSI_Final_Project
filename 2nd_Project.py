1b091b6c61418fd429684481e1e92642e6af2305
Pratama Yoga <pratamayo16@gmail.com>
2019-04-28 08:59:55 +0700

    unimportant update

--- 2nd_Project.py
+++ 2nd_Project.py
@@ -1,5 +1,5 @@
 import turtle
-
+import numpy as np
 
 M = 100
 p = 0.3
@@ -9,17 +9,29 @@ dt = 1
 vmax = 5
 tmax = 100
 tr = []
+v = 0
+
 
 for i in range(10) :
     tr.append(turtle.Turtle())
     tr[i].up()
     tr[i].ht()
-
-for j in range(10):
-    tr[j].st()
-    tr[j].setx(-200)
-    tr[j].speed('slowest')
-    tr[j].fd(400)
-    print(tr[j].pos() - tr[j+1].pos()) 
+for t in range(tmax):
+    for j in range(10):
+        tr[j].st()
+        tr[j].setx(-200)
+        if (i == 0):
+            d = 0
+        else:
+            d = tr[j].xcor() - tr[j-1].xcor()
+        v = min(v+1, vmax)
+        print(d)
+        v = min(v,d)
+        prob = np.random.choice(np.arange(0, 2), p=[0.7,0.3])
+        v = max(0,prob)
+        print(v)
+        
+        tr[j].speed(v)
+        tr[j].fd(400)
     
 
