5ad0004ba5460c7952fb5879f852a9b984e4c770
Pratama Yoga <pratamayo16@gmail.com>
2019-04-28 16:22:56 +0700

    new update, but still freak in updateV

--- 2nd_Project.py
+++ 2nd_Project.py
@@ -1,37 +1,70 @@
 import turtle
 import numpy as np
+import random as rd
 
-M = 100
+M = 1000
 p = 0.3
 v0 = 0
 N = 10
 dt = 1
-vmax = 5
+vmax = 50
 tmax = 100
+t = 0
 tr = []
-v = 0
+v = []
+v = [v0]*N
+vCar = []
+clr = ['red','green','blue','black','yellow','orange','magenta','purple','pink','aquamarine']
 
+turtle.Screen().setup(1000,300,0,0)
+turtle.Screen().delay(1)
 
-for i in range(10) :
-    tr.append(turtle.Turtle())
-    tr[i].up()
-    tr[i].ht()
-for t in range(tmax):
-    for j in range(10):
-        tr[j].st()
-        tr[j].setx(-200)
-        if (i == 0):
-            d = 0
+def jarakMobil() :
+    if (k == N-1):
+        if(tr[k].xcor() > tr[0].xcor()):
+           d = M - tr[i].xcor() + tr[0].xcor()
+        else :
+           d = tr[0].xcor() - tr[k].xcor()
+    else :
+        if (tr[k].xcor() < tr[k+1].xcor()) :
+            d = tr[k+1].xcor() - tr[k].xcor()
         else:
-            d = tr[j].xcor() - tr[j-1].xcor()
-        v = min(v+1, vmax)
-        print(d)
-        v = min(v,d)
-        prob = np.random.choice(np.arange(0, 2), p=[0.7,0.3])
-        v = max(0,prob)
-        print(v)
+            d = (M-1) - tr[k+1].xcor() + tr[k].xcor()
+    return d
         
-        tr[j].speed(v)
-        tr[j].fd(400)
+def updateV():
+    v[k] = min(v[k]+1, vmax)
+    d = jarakMobil()
+    v[k] = min(v[k], d-1)
+    x = np.random.uniform(0,1)
+    if (x <= p):
+        v[k] = max(0,v[k]-1)
+    return v[k]
+
+
+for j in range(10) :
+    tr.append(turtle.Turtle())
+
+
+for i in range (10) :
+    x = rd.randint(-500,500)
+    tr[i].color(clr[i])
+    tr[i].penup()
+    tr[i].goto(x,0)
     
+
+while True:
+    for k in range(10):
+        tr[k].forward(updateV())
+        tr[k].speed(1)
+        if (tr[k].xcor() >= 500):
+            tr[k].ht()
+            tr[k].goto(-500,0)
+            tr[k].st()
+    t+=1
+    print(v)
+    
+    
+    
+        
 
